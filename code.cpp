c5596cf6ab3193d2589c16c70a9228c3a022c943
itztempr <102239714+itztempr@users.noreply.github.com>
2022-05-29 20:45:06 +0300

    Create code.cpp

--- code.cpp
+++ code.cpp
@@ -1,8 +1,6 @@
 //i guess here is where the magic will happen
 #include <iostream>
 #include <vector>
-#include "bst.hpp"
-#include "min-heap.hpp"
 
 using namespace std;
 
@@ -14,5 +12,21 @@ int main() {
         int random_number = min + rand() % ((max - min) +1);
         rvec.push_back(random_number);
     }
+    while(true) {
+        int k;
+        cout <<"1. Populate values to BST"<<endl;
+        cout <<"2. Populate values to min-heap"<<endl;
+        cout <<"3. Sort desc using BST and print the first 20 values"<<endl;
+        cout <<"4. Sort desc using min-heap and print the first 20 values"<<endl;
+        cout <<"5. Print Summary"<<endl;
+        cout <<"6. Exit"<<endl;
+        cout <<"Enter your choice (value between 1-6):"<<endl;
+        cin>>k;
+        if(k == 6) {
+            break;
+        }
+
+  
+            }
     return 0;
 }
